b555a36a6d74756d7e78557039f3f1e41890bbbe
Sanat Kumar Dubey <studentia312@gmail.com>
2021-05-23 09:39:51 +0530

    inserting the value at the middle Node.java

--- src/Node.java
+++ src/Node.java
@@ -15,6 +15,7 @@ class SinglyLinkedList {
     public SinglyLinkedList() {
         this.head = null;
     }
+//    inserting the value at the end of the list
     public void insertAtEnd(Node newNode){
         if (head==null){
             head=newNode;
@@ -25,9 +26,39 @@ class SinglyLinkedList {
                 temp=temp.next;
             }
             temp.next=newNode;
-        }
-
+        } }
+//    inserting the value at the begining of the list
+    public void insertAtBegin(Node newNode){
+        newNode.next=head;
+        head=newNode;
     }
+//    inserting the value at the middle of the list
+    public void insertAtTheMiddle(Node newNode){
+        if(head==null){
+            head=newNode;
+        }
+        else if(head.next==null){
+            head.next=newNode;
+        }
+        else{
+            Node temp=head;
+            int count=0;
+            while(temp!=null){
+                count+=1;
+                temp=temp.next;
+            }
+            int middle=count/2;
+            int i =1;
+            while (i<middle){
+                temp=temp.next;
+                i+=1;
+            }
+            Node newnode=newNode;
+            newnode.next=temp.next;
+            temp.next=newnode;
+        }
+    }
+//    for printing the list
     public void print(){
         if(head!=null){
             Node temp=head;
@@ -37,10 +68,6 @@ class SinglyLinkedList {
             }
             System.out.println();
         }
-    }
-    public void insertAtBegin(Node newNode){
-        newNode.next=head;
-        head=newNode;
     }
 }
 class Main{
