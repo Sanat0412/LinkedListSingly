a1e58ea5cab0c39bd3ad2f593356a1d1c7f03c21
Sanat Kumar Dubey <68190043+Sanat0412@users.noreply.github.com>
2022-10-09 22:07:30 +0530

    Update RemoveDuplicate.java
    best one

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -1,25 +1,47 @@
-package LinkedList.SinglyLinkedList;
-public class RemoveDuplicate extends SinglyLinkedlist {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
-    //remove duplicates from sorted linked list
-    public ListNode removeDuplicate(){
-        ListNode current=head;
-        while(current!=null && current.next!=null){
-            if(current.data==current.next.data){
-                current.next=current.next.next;
+public class A_Array_Rearrangment {
+    public static void main(String[] args ){
+        Scanner sc=new Scanner(System.in);
+        int t= sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int x= sc.nextInt();
+            int[] arr=new int[n];
+            int[] brr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            for(int i=0;i<n;i++){
+                brr[i]=sc.nextInt();
+            }
+            Arrays.sort(arr);
+            Arrays.sort(brr);
+            int temp=0;
+            for (int i = 0; i < brr.length; i++) {
+                for (int j = i+1; j < brr.length; j++) {
+                    if(arr[i] < arr[j]) {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            int count=0;
+            for (int i = 0; i < n; i++) {
+                if (arr[i]+brr[i]<=x){
+                    count++;
+                }
+            }
+            if(count==n){
+                System.out.println("YES");
             }
             else{
-                current=current.next;
+                System.out.println("NO");
             }
-            return current;
-        
         }
     }
-
-
-
-    public static void main(String[] args) {
-        
-    }
-    
 }
